b4a6573f098231c541ea1abad1c0b4f1c0249b02
SAMALA VIJAY <vijaysamala18@gmail.com>
2026-01-20 00:09:32 +0530

    Add number guessing game functionality
    Implemented a number guessing game that generates a random number between 1 and 1000 and prompts the user to guess it, providing feedback on whether the guess is too high or too low.

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-
 int main() {
-    cout << "Hello CodSoft!" << endl;
-    cout << "C++ Internship Task 1" << endl;
+    int num, guess = 0;
+    srand(time(0));
+    num = rand() % 1000 + 1;
+cout << " Number Guessing Game " << endl;
+    cout << "Guess a number between 1 and 1000" << endl;
+
+    while (guess != num) {
+        cout << "Enter your number:";
+        cin >> guess;
+
+        if (guess > num) {
+            cout << " your guess is Too high,try again." << endl;
+        }
+        else if (guess < num) {
+            cout << "your guess is Too low, try again." << endl;
+        }
+        else {
+            cout << "Congratulations,Your guess is correct." << endl;
+        }
+}
+
     return 0;
 }
